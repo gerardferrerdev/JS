7433618fbffc94bf537d7f6fbc5e9dd8b1588a29
Gerard Ferrer <ferrergerard112@gmail.com>
2025-08-07 08:12:41 -0300

    funções

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//variaveis
 let myNickname = "Sr.Ferrer";
 const myName = "Gerard";
 console.log("Nome: " + myName);
@@ -8,6 +8,9 @@ console.log("Apelido: " + myNickname);
 const newName = myName + " Ferrer";
 console.log("Nome e Sobrenome: "+newName);
 
+console.log("--------------------------------------------------------------------------")
+
+//concatenação
 let age = 18;
 console.log("Idade de "+newName+": "+age+" anos");
 
@@ -20,13 +23,15 @@ let notboksBroken = 1;
 let myNotbooks = nunbsOfNotbooks - notboksBroken;
 console.log("hoje "+newName+" tem "+myNotbooks+" notbook");
 
+console.log("--------------------------------------------------------------------------")
 
 let price = 300;
 console.log(price);
 price += 50;
 console.log(price);
 
-//ternario
+// Operadores Ternario
+console.log("--------------------------------------------------------------------------")
 
 const ageMauricio = 200;
 console.log("É",
@@ -37,6 +42,8 @@ console.log("É",
     "anciaõ?????"
 );
 
+//estrutura condicional if/else
+console.log("--------------------------------------------------------------------------")
 
 let note = 1;
 
@@ -47,6 +54,8 @@ if (note < 6) {
 }else{
     console.log("Parabens, passou com exito!!!");
 }
+console.log("--------------------------------------------------------------------------")
+
 
 let sideA = 3;
 let sideB = 1;
@@ -59,3 +68,55 @@ if (sideA === sideB && sideB ===sideC && sideC === sideA) {
 }else{
     console.log("Este é um tringulo escaleno");
 }
+
+//Estrutura condicional Switch
+console.log("--------------------------------------------------------------------------")
+
+let enimal = "vaca"
+
+switch (enimal){
+    case "gato":{
+        console.log("miau")
+        break
+    }
+    case "galinha":{
+        console.log("cocorico")
+        break
+    }
+    case "cachorro":{
+        console.log("au au")
+    }
+    case "vaca":{
+        console.log("muuuuuuuuuuhhhhhh")
+    }
+    }
+
+    console.log("--------------------------------------------------------------------------")
+// estrutura de repetição "for( ){ }"
+console.log("-----Tabuada-----");
+let tabuadeDe = 358;
+for (let i=1 ; i<=10 ; i++){
+    let resultadoTabuada = tabuadeDe*i
+    console.log(tabuadeDe+" x "+i+ "= "+resultadoTabuada)
+}
+console.log("-----------------");
+
+console.log("--------------------------------------------------------------------------")
+//Estrutura de repetição while
+
+let num = 1;
+let tabudaDe2 = 10;
+while (num <=10){
+    console.log(`${tabudaDe2} x ${num} = ${tabudaDe2*num}`)
+    num++
+}
+console.log("--------------------------------------------------------------------------")
+//Função
+function greet(name){
+    console.log("Ola, seja bem vindo",name);
+}
+greet("Reiden");
+
+console.log("--------------------------------------------------------------------------")
+
+
